22b331e80fb14ffb7dd10db835df7dbb6efa31ff
Vikas Shukla <shuklaboy452@gmail.com>
2023-12-29 02:18:12 +0530

    THIS IS for crud code

--- crud.js
+++ crud.js
@@ -5,6 +5,18 @@ const path=require('path')
 const dirPath=path.join(__dirname,'Crud');
 const filePath=`${dirPath}/apple.txt`;
 
-// fs.writeFileSync(filePath,'This is a simple')
+fs.writeFileSync(filePath,'This is a simple')
 
-fs.readFile(filePath,())
+fs.readFile(filePath,'utf8',(err,item)=>{
+console.log(item);
+})
+
+fs.appendFile(filePath,' vikas',(err)=>{
+if(!err) console.log("file is updated")
+})
+
+fs.rename(filePath, `${dirPath}/fruit.txt`,(err)=>{
+if(!err) console.log("file name is updated")
+})
+
+fs.unlinkSync(`${dirPath}/fruit.txt`);
