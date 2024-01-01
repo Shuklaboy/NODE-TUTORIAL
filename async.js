4bcdd824d53a0549cace4684af885b5c8bec595e
Vikas Shukla <shuklaboy452@gmail.com>
2024-01-01 23:41:19 +0530

    promises

--- async.js
+++ async.js
@@ -1,8 +1,14 @@
-let a=20;
-let b=0;
+let a = 20;
+let b = 0;
 
-setTimeout(()=>{
-      b=30;
-   },2000)
 
-console.log(a+b)
+let waitingData = new Promise((resolve, reject) => {
+   setTimeout(() => {
+      resolve(30);
+   }, 2000)
+})
+
+waitingData.then((data) => {
+   b = data;
+   console.log(a + b)
+})
