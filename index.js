2ca8e2c2865dc3056ba7deb4afc283fc4f29d4d4
Vikas Shukla <shuklaboy452@gmail.com>
2024-01-11 19:28:53 +0530

    HTML render with express #20

--- index.js
+++ index.js
@@ -2,15 +2,32 @@ const express = require('express');
 const app = express();
 
 app.get("", (req, resp) => {
-    resp.send("Welcome,"+req.query.name);
+    console.log(req.query.name)
+    resp.send(`
+    <h1>Welcome</h1> <a href="/about"> Go to about Page </a>
+    
+    `);
 });
 
 app.get("/about", (req, resp) => {
-    resp.send("Welcome, This is a About Page");
+    resp.send(`
+<input type ="text" placeholder="User name "/>
+<button>Click Me</button>
+<a href="/"> Go to home Page </a>
+    `);
 });
 
 app.get("/help", (req, resp) => {
-    resp.send("Welcome, This is a Help Page");
+    resp.send([{
+        name:'Vikas Shukla',
+        email:'email@test.com'
+
+    },
+    {
+        name:'Shukla',
+        email:'mail@test.com'
+
+    }]);
 });
 
 app.listen(5000);
