f08aa84fd701ae9569718d61fd1fe243b0602636
Vikas Shukla <shuklaboy452@gmail.com>
2024-01-12 18:21:54 +0530

    21 AND 22 , express html page render

--- data.js
+++ data.js
@@ -1,6 +1,9 @@
+const express = require('express');
+const app = express();
 const data=[
     {name:'Vikas Shukla',phone : 8360265565},
     {name:'Shukla',phone : 8360},
     {name:'akash',phone : 5565}
 ]
 module.exports=data;
+app.listen(4000);
--- index.js
+++ index.js
@@ -1,33 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get("", (req, resp) => {
-    console.log(req.query.name)
-    resp.send(`
-    <h1>Welcome</h1> <a href="/about"> Go to about Page </a>
-    
-    `);
-});
+const publicPath=path.join(__dirname,'public')
 
-app.get("/about", (req, resp) => {
-    resp.send(`
-<input type ="text" placeholder="User name " value="${req.query.name}"/>
-<button>Click Me</button>
-<a href="/"> Go to home Page </a>
-    `);
-});
+app.use(express.static(publicPath));
 
-app.get("/help", (req, resp) => {
-    resp.send([{
-        name:'Vikas Shukla',
-        email:'email@test.com'
-
-    },
-    {
-        name:'Shukla',
-        email:'mail@test.com'
-
-    }]);
-});
 
 app.listen(5000);
