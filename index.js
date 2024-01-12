1c4b586f2a8ec7ec0c79851700d5295c7e557ce0
Vikas Shukla <shuklaboy452@gmail.com>
2024-01-12 18:53:53 +0530

    22 AND 23

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const path = require('path');
 
 const publicPath=path.join(__dirname,'public')
 
-app.use(express.static(publicPath));
-
+// app.use(express.static(publicPath));
+app.get('',(_,resp)=>{
+    resp.sendFile(`${publicPath}/home.html`)
+})
+app.get('*',(_,resp)=>{
+    resp.sendFile(`${publicPath}/error.html`)
+})
 
 app.listen(5000);
