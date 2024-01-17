eb80a8bdade3b3f8e50859738b547f6b25ca8fbc
Vikas Shukla <shuklaboy452@gmail.com>
2024-01-17 23:58:30 +0530

    hh

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set ('view engine','ejs')
 app.get('/profile',(_,resp)=>{
     const user ={
         name: 'vikas shukla',
-        city:'JODHPUR'
+        city:'J'
     }
     resp.render('profile',{user})
 })
